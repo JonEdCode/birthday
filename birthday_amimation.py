49f42236328548cec02c41e1aba10b8a6cf4cd43
JonEdCode <monika_gouws@yahoo.co.uk>
2025-03-17 19:07:28 +0000

    Add files via upload

---
+++ birthday_amimation.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+WIDTH = 600
+HEIGHT = 600
+TITLE = "birthday"
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("birthday")
+bd = pygame.image.load("birthday.jpg")
+bd2 = pygame.image.load("cake.jpg")
+bd3 = pygame.image.load("presant.jpg")
+run = True
+while run  == True:
+    screen.blit(bd,(0,0))
+    font = pygame.font.SysFont("sherif",50)
+    words = font.render("happy birthday!",True,"black")
+    screen.blit(words,(180,300))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False 
+        
+
+    pygame.display.update()
