c7cd7773d98112d885e1fce21ebc5c912d99b383
JonEdCode <monika_gouws@yahoo.co.uk>
2025-03-31 18:33:52 +0100

    Add files via upload

--- birthday_amimation.py
+++ birthday_amimation.py
@@ -15,9 +15,22 @@ while run  == True:
     font = pygame.font.SysFont("sherif",50)
     words = font.render("happy birthday!",True,"black")
     screen.blit(words,(180,300))
+    pygame.display.update()
+    
+    pygame.time.delay(3000)
+    screen.blit(bd2,(0,0))
+    pygame.display.update()
+
+    pygame.time.delay(3000)
+    screen.blit(bd3,(0,0))
+    pygame.display.update()
+    pygame.time.delay(3000)
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
-        
+
 
     pygame.display.update()
